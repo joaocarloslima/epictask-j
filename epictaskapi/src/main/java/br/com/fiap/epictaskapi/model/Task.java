6922897d4271fff2dad56ae202796454092f9c59
Joao <joaocarloslima@me.com>
2022-08-09 09:45:17 -0300

    :wastebasket: Spring Data

--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/model/Task.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/model/Task.java
@@ -1,15 +1,39 @@
 package br.com.fiap.epictaskapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Task {
     
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank
     private String title;
+    
+    @Size(min = 10, message = "melhore essa descrição")
     private String description;
     private int score = 100;
     private int status = 0;
 
+    public Task(){}
+    
     public Task(String title, String description) {
         this.title = title;
         this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -35,6 +59,12 @@ public class Task {
     }
     public void setStatus(int status) {
         this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task [description=" + description + ", id=" + id + ", score=" + score + ", status=" + status
+                + ", title=" + title + "]";
     }
 
     
--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/service/TaskService.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/service/TaskService.java
@@ -3,19 +3,28 @@ package br.com.fiap.epictaskapi.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fiap.epictaskapi.model.Task;
+import br.com.fiap.epictaskapi.repository.TaskRepository;
 
 @Service
 public class TaskService {
     
+    @Autowired
+    TaskRepository repository;
+    
     public List<Task> listAll(){
-        return List.of( 
-            new Task("Modalagem do BD", "Modelagem das tabelas do banco de dados"),
-            new Task("Prototipação", "Prototipar a interface gráfica")
-        );
+        return repository.findAll();
+    }
 
+    public void save(Task task){
+        repository.save(task);
+    }
+
+    public Task get(Long id){
+        return repository.getReferenceById(id);
     }
 
 }
